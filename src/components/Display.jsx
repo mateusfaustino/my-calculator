3405b09850f6e2ad83a60ec3cdcf4617cc3b258e
Mateus Faustino <mateusfaustino2017@gmail.com>
2021-02-11 09:14:18 -0300

    history

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -6,20 +6,36 @@ export default props =>{
     const lastItemOfArray= listOfOperations.length-1
     
     const lastOperation1 = listOfOperations[lastItemOfArray].firstValue + '+' + listOfOperations[lastItemOfArray].secondValue 
-    const lastResult1 = listOfOperations[lastItemOfArray].result
+        
     const lastOperation2 = listOfOperations[lastItemOfArray-1].firstValue + '+' + listOfOperations[lastItemOfArray-1].secondValue
-    
+    const lastResult2 = listOfOperations[lastItemOfArray-1].result
+
     const lastOperation3 = listOfOperations[lastItemOfArray-2].firstValue + '+' + listOfOperations[lastItemOfArray-2].secondValue
-    
+    const lastResult3 = listOfOperations[lastItemOfArray-2].result
+
     const lastOperation4 = listOfOperations[lastItemOfArray-3].firstValue + '+' + listOfOperations[lastItemOfArray-3].secondValue
+    const lastResult4 = listOfOperations[lastItemOfArray-3].result
+    
     console.log("list", listOfOperations)
    
     return(
         <div className="display-container">
-            <h4>{lastOperation4}</h4>
-            <h3>{lastOperation3}</h3>
-            <h2>{lastOperation2}</h2>
-            <h1>{lastOperation1}</h1>
+            <div className="last-operation last-operation4">
+                <h5>{lastOperation4}</h5>
+                <h5>={lastResult4}</h5>
+            </div>
+            <div className="last-operation last-operation3">
+                <h4>{lastOperation3}</h4>
+                <h4>={lastResult3}</h4>
+            </div>
+            <div className="last-operation last-operation2">
+                <h3>{lastOperation2}</h3>
+                <h3>={lastResult2}</h3>
+            </div>
+            <div className="last-operation last-operation1">
+                <h2>{lastOperation1}</h2>
+            </div>
+
             <div className="display">={props.value}</div>
         </div>    
     )
